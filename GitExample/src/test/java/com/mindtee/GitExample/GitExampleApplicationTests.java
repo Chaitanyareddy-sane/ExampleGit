458161b255a6262e303067e3ebac89aeab0920d2
mindc1jun94 <mindc1jun94@c1jun77.iiht.tech>
2022-08-10 12:40:14 +0530

    test commit

---
+++ GitExample/src/test/java/com/mindtee/GitExample/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtee.GitExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
